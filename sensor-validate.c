1ab74620698a2b39ab86be03ce06a44089ac67e3
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-23 22:39:17 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -6,27 +6,7 @@ int isVariationInRange(double value, double nextValue, double maxDelta) {
   }
   return 1;
 }
-#if 0
-int validateSOCreadings(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.05)) {
-      return 0;
-    }
-  }
-  return 1;
-}
 
-int validateCurrentreadings(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.1)) {
-      return 0;
-    }
-  }
-  return 1;
-}
-#endif
 
 int validateParameterreadings(double* values,  int numOfValues , sensor_type_en sensor_type)
 {
