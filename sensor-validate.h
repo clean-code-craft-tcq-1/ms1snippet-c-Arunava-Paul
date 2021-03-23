c83af9cff5a1a03f47bc187d0e0c9682dbc4164a
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-23 22:36:53 +0530

    Update sensor-validate.h

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,6 +1,18 @@
+typedef enum
+{
+	SOC_sensor=0,
+	Current_sensor,
+	Max_count_sensor
+}sensor_type_en;
 
-int _give_me_a_good_name(double value, double nextValue, double maxDelta);
+typedef struct
+{
+	double max_allowed_delta;
+}sensor_eval_parameter_st;
 
-int validateSOCreadings(double* values, int numOfValues);
+int validateParameterreadings(double* values,  int numOfValues , sensor_type_en sensor_type);
+int isVariationInRange(double value, double nextValue, double maxDelta);
 
-int validateCurrentreadings(double* values, int numOfValues);
+//int validateSOCreadings(double* values, int numOfValues);
+
+//int validateCurrentreadings(double* values, int numOfValues);
