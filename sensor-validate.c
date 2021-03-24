49ba0de56f1095a98e23dac60c163bb987a29a59
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-24 11:36:38 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -1,5 +1,5 @@
 #include "sensor-validate.h"
-
+#include "stdio.h"
 int isVariationInRange(double value, double nextValue, double maxDelta) {
   if(nextValue - value > maxDelta) {
     return 0;
@@ -10,12 +10,12 @@ int isVariationInRange(double value, double nextValue, double maxDelta) {
 
 int validate_SensorParameterReadings(double* values,  int numOfValues , sensor_type_en sensor_type)
 {
-	bool retVal = 0; /*Set to not ok*/
+	int i,retVal = 0; /*Set to not ok*/
 	const sensor_eval_parameter_st sensor_eval_parameter[Max_count_sensor]={{0.05},{0.1}};	
 	if((values != 0 ) && (numOfValues > 1))//check the null pointer case
 	{
 		int lastButOneIndex = numOfValues - 1;
-		for(int i = 0; i < lastButOneIndex; i++) 
+		for(i = 0; i < lastButOneIndex; i++) 
 		{
 			if(isVariationInRange(values[i], values[i + 1], sensor_eval_parameter[sensor_type].max_allowed_delta)) 
 			{
