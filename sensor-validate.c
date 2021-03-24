d6d31b5f406cd3676f2f019f4391b9a0e3b3a249
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-24 22:07:36 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -10,18 +10,30 @@ int isVariationInRange(double value, double nextValue, double maxDelta) {
 
 int validate_SensorParameterReadings(double* values,  int numOfValues , sensor_type_en sensor_type)
 {
-	int i,retVal = 1; /*Set to not ok*/
+	int i ;
 	const sensor_eval_parameter_st sensor_eval_parameter[Max_count_sensor]={{0.05},{0.1}};	
-	if((values != 0 ) && (numOfValues > 1))//check the null pointer case
+	if(!isSanityCheckOk(values , numOfValues))//check the null pointer case
 	{
-		int lastButOneIndex = numOfValues - 1;
-		for(i = 0; i < lastButOneIndex; i++) 
+		return 0;
+	}
+	int lastButOneIndex = numOfValues - 1;
+	for(i = 0; i < lastButOneIndex; i++) 
+	{
+		if(!isVariationInRange(values[i], values[i + 1], sensor_eval_parameter[sensor_type].max_allowed_delta)) 
 		{
-			if(!isVariationInRange(values[i], values[i + 1], sensor_eval_parameter[sensor_type].max_allowed_delta)) 
-			{
-				retVal = 0;/*Set to ok*/
-			}
+			return 0;/*Set to ok*/
 		}
 	}
-	return retVal;
+	
+	return 1;
 }/*end of validate_SensorParameterReadings*/
+
+
+int isSanityCheckOk(double* values,  int numOfValues)
+{
+	if((values != 0 ) && (numOfValues > 1))
+	{
+		return 0;
+	}
+	return 1;
+}
